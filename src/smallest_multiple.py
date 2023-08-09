5797087c42a6ef482d49c05f04cbcbedc45670b5
talhajamal11 <talhajamal11@gmail.com>
2023-08-09 11:17:56 +0500

    Solved question to find the smallest multiple of a list of numbers using Euclidean Algorithm to find the Greatest Common Divisor, a formula to find the LCM, and a function to iterate over a list of numbers and find their LCM.

--- src/smallest_multiple.py
+++ src/smallest_multiple.py
@@ -9,14 +9,21 @@ def greatest_common_divisor(num1: int, num2: int) -> int:
     Algorithm to find the Greatest Common Divisor for two numbers
 
     Euclidean Algorithm:
-    
+    Take larger problem and divide it into subproblems
+    GCD(A,0) = A
+    GCD(0,B) = B
+    If A = B⋅Q + R (found by taking modules)
+    and B≠0 then GCD(A,B) = GCD(B,R) where Q is an integer, R is an integer between 0 and B-1
+    Loop until second number becomes 0, Assign second number to first number
+    Assign the remainder of number1 / number 2 to number 2
+    Repeat till second number = 0
+    Return First Number
     """
     while num2:
         temp = num1
         num1 = num2
         num2 = temp % num2
     return num1
-
 
 def lcm(num1: int, num2: int) -> int:
     """
@@ -26,4 +33,20 @@ def lcm(num1: int, num2: int) -> int:
     """
     return (num1 * num2) // greatest_common_divisor(num1, num2)
 
-print(lcm(10, 24))
+def lcm_multiple_numbers(numbers: list) -> int:
+    """
+    Take a list of numbers and find the LCM 
+    """
+    if len(numbers) == 0:
+        return None
+    if len(numbers) == 1:
+        return numbers[0]
+
+    result = numbers[0]
+    for i in range(1, len(numbers)):
+        result = lcm(result, numbers[i])
+    return result
+
+arr = [i for i in range(1, 21)]
+print(arr)
+print(lcm_multiple_numbers(arr))
