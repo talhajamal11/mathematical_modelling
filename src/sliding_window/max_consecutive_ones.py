0f80b813229c99e8fecaf95d4376f155b6547171
talhajamal11 <talhajamal11@gmail.com>
2023-09-09 21:53:39 +0100

    Its all a pattern - sliding window and figuring out the right condition

---
+++ src/sliding_window/max_consecutive_ones.py
@@ -0,0 +1,24 @@
+"""
+Given a binary array nums and an integer k, 
+return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+"""
+
+numbers = [1,1,1,0,0,0,1,1,1,1,0]
+replacements = 2
+
+def longestOnes(nums: list, k: int) -> int:
+    """
+    return longest subarray with 1s
+    """
+    count = {}
+    left, length = 0, 0
+    for right, number in enumerate(nums):
+        count[number] = 1 + count.get(number, 0)
+        if ((right - left + 1) - (count.get(1, 0))) > k:
+            # Invalid condition - fix it till valid
+            count[nums[left]] -= 1
+            left += 1
+        length = max(length, right - left + 1)
+    return length
+
+print(longestOnes(numbers, replacements))
