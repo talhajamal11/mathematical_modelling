0954827a132b7c5e39801703ecb3d48ea43ddb93
talhajamal11 <talhajamal11@gmail.com>
2023-09-09 12:23:24 +0100

    MaxProfit

--- src/sliding_window/max_profit.py
+++ src/sliding_window/max_profit.py
@@ -1,14 +1,27 @@
 """
 Buy Low Sell High
 """
-input = [7, 1, 5, 3, 6, 4]
+prices = [1, 5, 3, 1, 8]
 
 def max_profit(array: list) -> any:
     """
     Returns Buy Date, Sell Date, and max profit
     """
-    l, r = 0, 1 # left = buy, right = sell 
-    while r <= len(array):
+    left, right = 0, 1 # left = buy, right = sell
+    profit = 0
+    buy, sell = 0, 0
+    while right < len(array):
         # Check for max profit
-        
+        if array[left] < array[right]:
+            # profitable
+            # is current profit higher than existing profit?
+            profit = max(profit, array[right] - array[left])
+            buy = array[left]
+            sell = array[right]
+        elif array[right] < array[left]:
+            # current price is lower - buy low
+            left = right
+        right += 1
+    return profit, buy, sell
 
+print(max_profit(prices))
