deb0409955cbeb9414e081e8acadbddbfdbbd2ec
talhajamal11 <talhajamal11@gmail.com>
2023-10-09 12:59:32 +0100

    Rewriting code

--- src/two_pointers/trapping_rainwater.py
+++ src/two_pointers/trapping_rainwater.py
@@ -9,21 +9,21 @@ def trap_water(height:list) -> int:
     """
     left, right = 0, len(height) - 1
     left_max, right_max, water = 0, 0, 0
+    # Left Max is the height of the tower on the left and vice versa
     while left < right:
-        if height[left] < height[right]: # can contain water
+        if height[left] < height[right]:
             if height[left] < left_max:
                 water += left_max - height[left]
             else:
                 left_max = height[left]
             left += 1
-        else: # height[left] > height[right]
-            if height[right] < right_max: # can contain water
+        else: # height[left] > height[right]:
+            if height[right] < right_max:
                 water += right_max - height[right]
             else:
                 right_max = height[right]
-            right -=1
+            right -= 1
     return water
 
-
-height = [4,2,0,3,2,5]
+height = [0,1,0,2,1,0,1,3,2,1,2,1]
 print(trap_water(height))
