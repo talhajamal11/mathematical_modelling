23400b55be8c814f420cc084c7c4bf3544a1154d
talhajamal11 <talhajamal11@gmail.com>
2023-10-30 20:49:40 +0000

    Updating questions I have seen so far

---
+++ src/Goldman Sachs Quant Strat Questions/max_min_product.py
@@ -0,0 +1,3 @@
+""" 
+Return a list containing the product of the maximum and minimum in a dataset
+"""
---
+++ src/JP Morgan Quant Questions/is_possible.py
@@ -0,0 +1,24 @@
+"""
+You are given (a, b). We can perform the following two actions:
+1. a + b, b
+2. a, b + a
+
+Determine if we can get to (c, d)
+"""
+
+def gcd(num1, num2):
+    while num2:
+        temp = num1
+        num1 = num2
+        num2 = temp % num2
+    return num1
+
+def is_possible(a,b,c,d):
+    print(gcd(a,b), gcd(c,d))
+    if gcd(a,b) == gcd(c,d):
+        return "Yes"
+    else:
+        return "No"
+
+
+print(is_possible(2,2,1000,998))
