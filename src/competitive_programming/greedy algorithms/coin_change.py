cdd12f9249de70fa817767bfa444da51a7d74adc
talhajamal11 <talhajamal11@gmail.com>
2023-10-31 18:42:12 +0000

    Solving coin change via dynamic and greeedy

--- src/competitive_programming/greedy algorithms/coin_change.py
+++ src/competitive_programming/greedy algorithms/coin_change.py
@@ -6,19 +6,31 @@ For example, if the coins are the euro coins (in cents) {1, 2, 5, 10, 20, 50, 10
 we need at least four coins. The optimal solution is to select coins 200+200+100+20 whose sum is 520.
 """
 
-coins = [1, 2, 5, 10, 20, 50, 100, 200]
-n = 520
+coins = [1, 3, 4]
+n = 6
 
-def min_coins(n, coins):
+def greedy_min_coins(n, coins):
     coins = sorted(coins)
     count = 0
     used = {}
     for i in range(len(coins) - 1, -1, -1):
-        while n - coins[i] > 0:
+        current_coin = coins[i]
+        while n - current_coin >= 0:
             count += 1
-            n -= coins[i]
-            used[coins[i]] = 1 + used.get(coins[i], 0)
+            n -= current_coin
+            used[current_coin] = 1 + used.get(current_coin, 0)
     return count
 
-print(min_coins(n, coins))
+print(greedy_min_coins(n, coins))
 
+def dp_min_coins(n, coins):
+    dp = [n + 1] * (n + 1)
+    dp[0] = 0
+    for amount in range(1, n + 1):
+        for c in coins:
+            if amount - c >= 0:
+                dp[amount] = min(dp[amount], 1 + dp[amount - c])
+    print(dp)
+    return dp[n] if dp[n] != n + 1 else -1
+
+print(dp_min_coins(n, coins))
