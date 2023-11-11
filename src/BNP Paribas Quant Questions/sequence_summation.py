28cdb10792df02e459bbbfd018009ca37c466210
talhajamal11 <talhajamal11@gmail.com>
2023-11-11 23:08:49 +0000

    Trust your fucking instint over ChatGPT

--- src/BNP Paribas Quant Questions/sequence_summation.py
+++ src/BNP Paribas Quant Questions/sequence_summation.py
@@ -1,11 +1,23 @@
 """ 
 You are given three integers: i, j, and k. Sum from i to j. Sum from j to k. Return total of the sums. 
 i, j, k can all be negative or positive. 
+Example: i = 5, j = 9, k = 6 - Result = 56
 """
 
 def seq_summation(i, j, k):
 
-    def sum_1_to_n(n):
-        return (n*(n+1))/2
+    def helper(num1, num2):
+        return(num2 - num1 + 1) * num1 + (num2 - num1)*(num2 - num1 + 1) / 2
+    
+    return int(helper(i, j) + helper(k, j-1))
 
-    print(sum_1_to_n())
+def solution(i, j, k):
+    def helper(num):
+        return (num*(num+1)) / 2
+    sum1 = helper(j) - helper(i-1)
+    sum2 = helper(j-1) - helper(k-1)
+    return int(sum1+sum2)
+
+print(solution(-5,5,-5))
+
+#print(seq_summation(-5,0,-5))
