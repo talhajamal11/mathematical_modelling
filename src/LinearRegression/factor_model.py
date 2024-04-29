e147b3df7a15441df0db0faa918a6684869f07dd
talhajamal11 <talhajamal11@gmail.com>
2024-04-29 12:10:14 +0100

    Compared TSLA with WMT

--- src/LinearRegression/factor_model.py
+++ src/LinearRegression/factor_model.py
@@ -6,17 +6,29 @@ from my_utils import download_stock_data
 from my_utils import linear_regression
 from my_utils import visualize_regression
 
-SYF = download_stock_data('SYF', start='2018-01-01', end='2019-01-01')
-SPY = download_stock_data('SPY', start='2018-01-01', end='2019-01-01')
+START = '2015-01-01'
+END = '2019-01-01'
 
-regression = linear_regression(x=SPY['Daily_Ret'],
-                               y=SYF['Daily_Ret'],
-                               x_ticker="SPY",
-                               y_ticker="SYF")
+SPY = download_stock_data('SPY', start=START, end=END)
 
+"""
 visualize_regression(SYF['Daily_Ret'],
                      SPY['Daily_Ret'],
                      regression,
                      title='SYF returns regressed on SPY returns',
                      xlabel='SPY',
                      ylabel='SYF')
+"""
+
+TSLA = download_stock_data('TSLA', start=START, end=END)
+
+tsla_regression = linear_regression(x=SPY['Daily_Ret'],
+                                    y=TSLA['Daily_Ret'],
+                                    x_ticker="SPY",
+                                    y_ticker="TSLA")
+
+WMT = download_stock_data('WMT', start=START, end=END)
+wmt_regression = linear_regression(x=SPY['Daily_Ret'],
+                                   y=WMT['Daily_Ret'],
+                                   x_ticker="SPY",
+                                   y_ticker="WMT")
